511905687c02e5a4f568e202a604f327ddd13587
Solatato <soltatoes@gmail.com>
2021-11-18 15:18:53 +0800

    a

--- src/views/Landing/components/Main/index.tsx
+++ src/views/Landing/components/Main/index.tsx
@@ -10,12 +10,12 @@ function Main() {
                 <img src={CatImg} alt="" />
             </div>
             <div className="landing-main-btns-wrap">
-                <Link href="https://app.taro.finance" target="_blank" rel="noreferrer">
+                <Link href="http://app.taro.finance" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
                         <p>Enter App</p>
                     </div>
                 </Link>
-                <Link href="https://wonderland.gitbook.io/wonderland/" target="_blank" rel="noreferrer">
+                <Link href="https://tarodao.gitbook.io/tarodao/" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
                         <p>Documentation</p>
                     </div>
